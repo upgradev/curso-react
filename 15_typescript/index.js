7cb59af6aea2c709a2a8517179939b03fbb3e039
Cleison França Amorim <>
2022-11-30 05:43:41 -0400

    estudos

---
+++ 15_typescript/index.js
@@ -0,0 +1,7 @@
+var firstName = "Ana";
+var anotherName = 1;
+var x = true;
+function greeting(name) {
+    console.log("Olá " + name);
+}
+greeting(firstName);
---
+++ 15_typescript/index.ts
@@ -0,0 +1,10 @@
+const firstName = "Ana";
+const anotherName = 1;
+const x = true;
+
+function greeting(name: string) {
+  console.log("Olá " + name);
+}
+
+greeting(firstName);
+
